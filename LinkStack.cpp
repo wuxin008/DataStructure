51ce852f42ef54ed4bdafedca53050b5b05daae9
NULL <62784553+wuxin008@users.noreply.github.com>
2021-10-22 23:00:59 +0800

    Update LinkStack.cpp

--- LinkStack.cpp
+++ LinkStack.cpp
@@ -12,17 +12,20 @@ typedef struct {
 	int age;
 }SElemType;
 
+//链栈
 typedef struct StackNode {
 	SElemType data;
 	StackNode* next;
 } StackNode, * LinkStack;
 
+//初始化链栈
 Status InitStack_L(LinkStack& S)
 {
 	S = NULL;
 	return OK;
 }
 
+//销毁链栈
 Status DestroyStack_L(LinkStack& S)
 {
 	StackNode* sp = S;
@@ -34,6 +37,7 @@ Status DestroyStack_L(LinkStack& S)
 	return OK;
 }
 
+//清空链栈
 Status ClearStack_L(LinkStack& S)
 {
 	StackNode* sp = S;
@@ -45,6 +49,7 @@ Status ClearStack_L(LinkStack& S)
 	return OK;
 }
 
+//判断链栈是否为空
 bool StackEmpty_L(LinkStack S)
 {
 	if (S)
@@ -53,6 +58,7 @@ bool StackEmpty_L(LinkStack S)
 		return true;
 }
 
+//求链栈长度
 int StackLength_L(LinkStack S)
 {
 	int count = 0;
@@ -64,6 +70,7 @@ int StackLength_L(LinkStack S)
 	return count;
 }
 
+//取链栈顶部元素
 Status GetTop_L(LinkStack S, SElemType& se)
 {
 	if (!S)
@@ -72,6 +79,7 @@ Status GetTop_L(LinkStack S, SElemType& se)
 	return OK;
 }
 
+//将元素压入链栈
 Status Push_L(LinkStack& S, SElemType se)
 {
 	StackNode* sp = new StackNode;
@@ -81,6 +89,7 @@ Status Push_L(LinkStack& S, SElemType se)
 	return OK;
 }
 
+//从链栈顶部取出链栈
 Status Pop_L(LinkStack& S, SElemType& se)
 {
 	if (!S)
@@ -92,7 +101,8 @@ Status Pop_L(LinkStack& S, SElemType& se)
 	return OK;
 }
 
-Status StackTraverse_L(LinkStack S)
+//遍历链栈
+Status StackTraverse(LinkStack S)
 {
 	if (S->next)
 		StackTraverse(S->next);
@@ -102,6 +112,7 @@ Status StackTraverse_L(LinkStack S)
 	return OK;
 }
 
+//测试
 int main()
 {
 	using namespace std;
@@ -114,14 +125,14 @@ int main()
 	Push_L(S, { "lisi", 12 });
 	Push_L(S, { "wangwu", 13 });
 	cout << "现在开始遍历栈\n";
-	StackTraverse_L(S);
+	StackTraverse(S);
 	cout << "弹出最后一个元素并输出：\n";
 	Pop_L(S, se);
 	std::cout << "My name is " << se.name
 		<< ". And I'm " << se.age
 		<< " years old." << std::endl;
 	cout << "现在开始遍历栈\n";
-	StackTraverse_L(S);
+	StackTraverse(S);
 	cout << "当前栈的长度为" << StackLength_L(S) << endl;
 	cout << "当前栈是否为空" << StackEmpty_L(S) << endl;
 	cout << "现在开始清空栈\n";
